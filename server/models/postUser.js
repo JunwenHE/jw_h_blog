8a11d7a6f172e425aa3873d1811e0c5b1787c9a8
Junwen HE <junwen_he@hotmail.com>
2022-01-28 18:16:13 +0800

    Add model

--- server/models/postUser.js
+++ server/models/postUser.js
@@ -2,8 +2,6 @@ import mongoose from 'mongoose';
 
 const userSchema = mongoose.Schema({
     creator: String,
-    tatitle: [String],
-    comments: [String],
 })
 
 var PostUser = mongoose.model('PostUser', userSchema);
